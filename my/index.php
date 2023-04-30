39bee38c3891451d664262d215ac8c7c4bbd024f
Ahmed Hosny <ahhosnyas@gmail.com>
2023-04-30 18:38:40 +0300

    disable dashboard

--- my/index.php
+++ my/index.php
@@ -37,6 +37,10 @@
 require_once(__DIR__ . '/../config.php');
 require_once($CFG->dirroot . '/my/lib.php');
 
+
+// redirect to home
+redirect(new moodle_url('/'));
+
 redirect_if_major_upgrade_required();
 
 // TODO Add sesskey check to edit
