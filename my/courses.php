8c17b2627c787b95bfd6a02cf571c2c140004fb6
Ahmed Hosny <ahhosnyas@gmail.com>
2023-05-12 10:23:10 +0300

    redirect to home if not admin

--- my/courses.php
+++ my/courses.php
@@ -31,6 +31,10 @@ require_once(__DIR__ . '/../config.php');
 require_once($CFG->dirroot . '/my/lib.php');
 require_once($CFG->dirroot . '/course/lib.php');
 
+if (!is_siteadmin()) {
+    redirect($CFG->wwwroot . '/');
+}
+
 redirect_if_major_upgrade_required();
 
 require_login();
